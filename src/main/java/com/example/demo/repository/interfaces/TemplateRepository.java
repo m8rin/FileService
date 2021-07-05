125f72f642ef3daae1cc29b65b035d97c093c1f2
m8rin <rinatrayan@mail.ru>
2021-07-05 18:18:04 +0500

    removed the Template class and everything related to it

--- src/main/java/com/example/demo/repository/interfaces/TemplateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository.interfaces;
-
-import com.example.demo.model.Template;
-import org.springframework.data.repository.CrudRepository;
-
-// Расширяя Spring CrudRepository, будем реализовывать некоторые методы для нашего репозитория данных.
-// Таким образом экономим много шаблонного кода.
-public interface TemplateRepository extends CrudRepository<Template, Integer> {
-}
