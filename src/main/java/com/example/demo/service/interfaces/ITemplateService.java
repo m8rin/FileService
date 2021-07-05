d0933cd8a851a774999fcaed4ae9f8335304909c
m8rin <rinatrayan@mail.ru>
2021-07-05 18:30:57 +0500

    fixup! removed the Template class and everything related to it

--- src/main/java/com/example/demo/service/interfaces/ITemplateService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service.interfaces;
-
-import com.example.demo.model.Template;
-
-import java.util.List;
-
-public interface ITemplateService {
-    List<Template> findAll();
-}
